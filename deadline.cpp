7d57b39924bd08cfd6b2371250bd5b97b18288b6
Lee Magnusson <lee@unhuman.io>
2019-04-02 10:30:20 -0700

    Use libudev to find the device

--- deadline.cpp
+++ deadline.cpp
@@ -14,6 +14,7 @@
 #include <cstring>
 #include <chrono>
 #include <fcntl.h>
+#include <libudev.h>
 
 #define gettid() syscall(__NR_gettid)
 
@@ -102,21 +103,22 @@ class CStack {
 	int pos_ = 0;
 };
 
-
+char * j1_dev_path;
 class Task {
  public:
     Task(CStack<Data> &cstack) : cstack_(cstack) {
 		// fid_ = fopen("/dev/skel0", "r");
 		// fid_out_ = fopen("/dev/skel1", "w");
-		fid_ = open("/dev/skel0", O_RDWR);
-		fid_out_ = open("/dev/skel1", O_WRONLY);
-		std::cout << "fidout" << fid_out_;
+		std::cout << "open: " << j1_dev_path << std::endl;
+		fid_ = open(j1_dev_path, O_RDWR);
+		//fid_out_ = open("/dev/skel1", O_WRONLY);
+		//std::cout << "fidout" << fid_out_;
 	}
 	~Task() {
 		// fclose(fid_);
 		// fclose(fid_out_);
 		close(fid_);
-		close(fid_out_);
+		//close(fid_out_);
 	}
 	void run() { done_ = 0;
 		start_time_ = std::chrono::steady_clock::now();
@@ -198,8 +200,95 @@ class Task {
 	int fid_, fid_out_;
 };
 
+int udev (void)
+{
+	struct udev *udev;
+	struct udev_enumerate *enumerate;
+	struct udev_list_entry *devices, *dev_list_entry;
+	struct udev_device *dev;
+	
+	/* Create the udev object */
+	udev = udev_new();
+	if (!udev) {
+		printf("Can't create udev\n");
+		exit(1);
+	}
+	
+	/* Create a list of the devices in the 'hidraw' subsystem. */
+	enumerate = udev_enumerate_new(udev);
+	udev_enumerate_add_match_sysname(enumerate, "skel*");
+	udev_enumerate_scan_devices(enumerate);
+	devices = udev_enumerate_get_list_entry(enumerate);
+	/* For each item enumerated, print out its information.
+	   udev_list_entry_foreach is a macro which expands to
+	   a loop. The loop will be executed for each member in
+	   devices, setting dev_list_entry to a list entry
+	   which contains the device's path in /sys. */
+	udev_list_entry_foreach(dev_list_entry, devices) {
+		const char *path;
+		
+		/* Get the filename of the /sys entry for the device
+		   and create a udev_device object (dev) representing it */
+		path = udev_list_entry_get_name(dev_list_entry);
+		const char * val = udev_list_entry_get_value(dev_list_entry);
+		dev = udev_device_new_from_syspath(udev, path);
+
+		/* usb_device_get_devnode() returns the path to the device node
+		   itself in /dev. */
+		printf("Device Node Path: %s, %s, %s\n", udev_device_get_devnode(dev), path, udev_device_get_sysname(dev));
+		printf("  name: %s\n",
+		         udev_device_get_sysattr_value(dev, "device/interface"));
+		if (strcmp("J1",udev_device_get_sysattr_value(dev, "device/interface")) == 0) {
+			std::cout << "strcmp J1 **********" << std::endl;
+			const char * devpath = udev_device_get_devnode(dev);
+			j1_dev_path = new char[std::strlen(devpath)];
+			std::strcpy(j1_dev_path, devpath);
+		}
+		/* The device pointed to by dev contains information about
+		   the hidraw device. In order to get information about the
+		   USB device, get the parent device with the
+		   subsystem/devtype pair of "usb"/"usb_device". This will
+		   be several levels up the tree, but the function will find
+		   it.*/
+		dev = udev_device_get_parent_with_subsystem_devtype(
+		       dev,
+		       "usb",
+		       "usb_device");
+		if (!dev) {
+			printf("Unable to find parent usb device.");
+			exit(1);
+		}
+	
+		/* From here, we can call get_sysattr_value() for each file
+		   in the device's /sys entry. The strings passed into these
+		   functions (idProduct, idVendor, serial, etc.) correspond
+		   directly to the files in the directory which represents
+		   the USB device. Note that USB strings are Unicode, UCS2
+		   encoded, but the strings returned from
+		   udev_device_get_sysattr_value() are UTF-8 encoded. */
+		printf("  VID/PID: %s %s\n",
+		        udev_device_get_sysattr_value(dev,"idVendor"),
+		        udev_device_get_sysattr_value(dev, "idProduct"));
+		printf("  %s\n  %s\n",
+		        udev_device_get_sysattr_value(dev,"manufacturer"),
+		        udev_device_get_sysattr_value(dev,"product"));
+		printf("  serial: %s\n",
+		         udev_device_get_sysattr_value(dev, "serial"));
+		printf("  name: %s\n",
+		         udev_device_get_sysattr_value(dev, "name"));
+		udev_device_unref(dev);
+	}
+	/* Free the enumerator object */
+	udev_enumerate_unref(enumerate);
+
+	udev_unref(udev);
+
+	return 0;       
+}
+
 int main (int argc, char **argv)
 {
+	udev();
 	printf("main thread [%ld]\n", gettid());
 	CStack<Data> cstack;
 	Task task(cstack);
