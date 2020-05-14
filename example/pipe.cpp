8c17b1d03c29831107fb480f99505af5d89e7cfc
Lee Magnusson <lee@unhuman.io>
2020-05-14 10:42:12 -0700

    arg

--- example/pipe.cpp
+++ example/pipe.cpp
@@ -18,7 +18,8 @@ class Task : public MotorThread {
 	virtual void pre_update() {
 		// check for data on the pipe, timeout before a usb read is likely to finish
 		pollfd pipe_fds[] = {{.fd=pipe_fd_, .events=POLLIN}};
-		timespec timeout_ts = {.tv_nsec=100 * 1000};
+		timespec timeout_ts ={};
+		timeout_ts.tv_nsec=100 * 1000;
 		int retval = ppoll(pipe_fds, 1, &timeout_ts, NULL);
 		if (retval) {
 			char data[motor_manager_.serialize_command_size()];
