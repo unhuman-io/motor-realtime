69c3a2b557e41c4c4bb554e596a3cc8b68a46459
Lee Magnusson <lee@unhuman.io>
2020-05-20 08:43:01 -0700

    One more cstack item

--- include/motor_thread.h
+++ include/motor_thread.h
@@ -17,7 +17,7 @@ struct Data {
 template <class T>
 class CStack {
  public:
-    void push(T &t) {
+    void push(T const &t) {
 		int future_pos = pos_.load(std::memory_order_acquire) + 1;
 		if (future_pos >= 100) {
 			future_pos = 0;
@@ -25,7 +25,7 @@ class CStack {
 		data_[future_pos] = t;
 		pos_.store(future_pos, std::memory_order_release);
 	}
-	T top() const {
+	T top() const { // return a copy of the data
 		return data_[pos_.load(std::memory_order_acquire)];
 	}
  private:
