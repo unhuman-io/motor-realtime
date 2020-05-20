b87e64574124ed98d5bee1d87d35525a82706ab8
Lee Magnusson <lee@unhuman.io>
2020-05-20 08:31:52 -0700

    further cstack stuff

--- include/motor_thread.h
+++ include/motor_thread.h
@@ -13,12 +13,12 @@ struct Data {
     std::chrono::steady_clock::time_point time_start, last_time_start, last_time_end, aread_time, read_time, control_time, write_time;
 };
 
-// A circular stack. If data is written by one thread and read by one other thread, data is read from the top without worrying about thread safety.
+// A circular stack. If data is written by one thread and read by one other thread then data is read from the top without worrying about thread safety.
 template <class T>
 class CStack {
  public:
     void push(T &t) {
-		int future_pos = pos_ + 1;
+		int future_pos = pos_.load(std::memory_order_acquire) + 1;
 		if (future_pos >= 100) {
 			future_pos = 0;
 		}
