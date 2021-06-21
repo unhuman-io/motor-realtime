b3878d943a6589aa8e1c2886903b580a55a7a18e
Lee Magnusson <lee@unhuman.io>
2021-06-21 11:21:28 -0700

    torque demo updates

--- python/torque_demo.py
+++ python/torque_demo.py
@@ -6,10 +6,10 @@ import time
 
 
 class TorqueDemo:
-    torque_value = 1    # Nm output
-    velocity_limit = 1  # rad/s output
-    wait_time = 5       # seconds
-    dt = 0.01           # seconds run frequency
+    torque_value = .3    # Nm output
+    velocity_limit = 2  # rad/s output
+    wait_time = 1       # seconds
+    dt = 0.1           # seconds run frequency
     motor_cpr = 2048*100    # counts per output revolution
 
     def __init__(self):
@@ -20,10 +20,10 @@ class TorqueDemo:
         self.set_hold_mode()
 
     def set_torque_mode(self):
-        self.state == "torque"
+        self.state = "torque"
         print("Torque mode")
         self.m.set_command_mode(motor.ModeDesired.Torque)
-        self.m.set_command_torque(self.torque_sign * self.torque_value)
+        self.m.set_command_torque([self.torque_sign * self.torque_value])
 
     def set_hold_mode(self):
         self.state = "hold"
@@ -43,29 +43,35 @@ class TorqueDemo:
         status = self.m.read()[0]
         last_encoder = status.motor_encoder
         last_mcu_time = status.mcu_timestamp
-        while(True):
-            # read motor
-            status = self.m.read()[0]
-            velocity = float(motor.diff_encoder(status.motor_encoder, last_encoder))/self.motor_cpr/ \
-                float(motor.diff_mcu_time(status.mcu_timestamp, last_mcu_time))*170e6
-            last_mcu_time = status.mcu_timestamp
-            last_encoder = status.motor_encoder
+        try:
+            while(True):
+                # read motor
+                status = self.m.read()[0]
+                velocity = float(motor.diff_encoder(status.motor_encoder, last_encoder))/self.motor_cpr/ \
+                    float(motor.diff_mcu_time(status.mcu_timestamp, last_mcu_time))*170e6
+                last_mcu_time = status.mcu_timestamp
+                last_encoder = status.motor_encoder
 
-            # update state
-            if (self.state == "hold"):
-                if (abs(status.torque) > self.torque_value):
-                    self.torque_sign = 1.0 if status.torque > 0 else -1.0
-                    self.set_torque_mode()
-            elif (self.state == "torque"):
-                if (abs(velocity) > self.velocity_limit):
-                    self.set_wait_mode()
-            elif (self.state == "wait"):
-                if (time.time() - self.t_start > self.wait_time):
-                    self.set_hold_mode()
-                
+                print("mode: {}, torque: {}, velocity: {}".format(self.state, status.torque, velocity))
+
+                # update state
+                if (self.state == "hold"):
+                    if (abs(status.torque) > self.torque_value):
+                        self.torque_sign = 1.0 if status.torque > 0 else -1.0
+                        self.set_torque_mode()
+                elif (self.state == "torque"):
+                    if (abs(velocity) > self.velocity_limit):
+                        self.set_wait_mode()
+                elif (self.state == "wait"):
+                    if (time.time() - self.t_start > self.wait_time):
+                        self.set_hold_mode()
+                    
+                self.m.write_saved_commands()
+                time.sleep(self.dt)
+        except KeyboardInterrupt:
+            print("keyboard interrupt, setting mode open")
+            self.m.set_command_mode(motor.ModeDesired.Open)
             self.m.write_saved_commands()
-            time.sleep(self.dt)
-
 
 if __name__ == "__main__":
     t = TorqueDemo()
