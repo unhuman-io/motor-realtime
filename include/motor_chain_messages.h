11cf9da7180f799fa432de8aebe08d5582e68d67
Lee Magnusson <lee@unhuman.io>
2022-01-25 17:09:10 -0800

    add more motor chain status helpers

--- include/motor_chain_messages.h
+++ include/motor_chain_messages.h
@@ -18,7 +18,11 @@ struct MotorChainStatus{
     typedef Eigen::Map<const Eigen::VectorXf, Eigen::Unaligned, 
         Eigen::InnerStride<sizeof(MotorStatus)/sizeof(float)>> ConstMotorStatusFloat;
     typedef Eigen::Map<const Eigen::Matrix<uint32_t, Eigen::Dynamic, 1>, Eigen::Unaligned, 
-        Eigen::InnerStride<sizeof(MotorStatus)/sizeof(float)>> ConstMotorStatusTimestamp;    
+        Eigen::InnerStride<sizeof(MotorStatus)/sizeof(uint32_t)>> ConstMotorStatusTimestamp;  
+    typedef Eigen::Map<const Eigen::Matrix<int32_t, Eigen::Dynamic, 1>, Eigen::Unaligned, 
+        Eigen::InnerStride<sizeof(MotorStatus)/sizeof(int32_t)>> ConstMotorStatusInt32;
+    typedef Eigen::Map<const Eigen::Matrix<uint8_t, Eigen::Dynamic, 1>, Eigen::Unaligned, 
+        Eigen::InnerStride<sizeof(MotorStatus)/sizeof(uint8_t)>> ConstMotorStatusUint8;        
  public:
     MotorChainStatus(std::vector<MotorStatus> &sv) :
         MotorChainStatus(sv.data(), sv.size()) {}
@@ -26,16 +30,25 @@ struct MotorChainStatus{
         size_(num_motors),
         data_(sa),
         mcu_timestamp_(&sa[0].mcu_timestamp, size()),
+        host_timestamp_received_(&sa[0].host_timestamp_received, size()),
         motor_position_(&sa[0].motor_position, size()),
         joint_position_(&sa[0].joint_position, size()),
         iq_(&sa[0].iq, size()),
-        torque_(&sa[0].torque, size()) {}
+        torque_(&sa[0].torque, size()),
+        motor_encoder_(&sa[0].motor_encoder, size()),
+        mode_(&sa[0].flags.mode, size()),
+        error_(reinterpret_cast<uint8_t*>(&sa[0].flags.error), size())
+        {}
     int size() const { return size_; }
     ConstMotorStatusTimestamp const& mcu_timestamp() const { return mcu_timestamp_; }
+    ConstMotorStatusTimestamp const& host_timestamp_received() const { return host_timestamp_received_; }
     ConstMotorStatusFloat const& motor_position() const { return motor_position_; }
     ConstMotorStatusFloat const& joint_position() const { return joint_position_; }
     ConstMotorStatusFloat const& iq() const { return iq_; }
     ConstMotorStatusFloat const& torque() const { return torque_; }
+    ConstMotorStatusInt32 const& motor_encoder() const { return motor_encoder_; }
+    ConstMotorStatusUint8 const& mode() const { return mode_; }
+    ConstMotorStatusUint8 const& error() const { return error_; }
     Eigen::VectorXd dt_seconds(const MotorChainStatus& last) {
         //TODO unhardcode cpu frequency
         return (mcu_timestamp() - last.mcu_timestamp()).cast<double>()/170e6; 
@@ -50,10 +63,14 @@ struct MotorChainStatus{
     int size_;
     MotorStatus *data_;
     ConstMotorStatusTimestamp mcu_timestamp_;
+    ConstMotorStatusTimestamp host_timestamp_received_;
     ConstMotorStatusFloat motor_position_;
     ConstMotorStatusFloat joint_position_;
     ConstMotorStatusFloat iq_;
     ConstMotorStatusFloat torque_;
+    ConstMotorStatusInt32 motor_encoder_;
+    ConstMotorStatusUint8 mode_;
+    ConstMotorStatusUint8 error_;
 };
 
 struct MotorChainCommand {
