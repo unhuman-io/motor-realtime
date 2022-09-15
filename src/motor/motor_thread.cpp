c70b49e706968e1331b6035db91b0e49b56908f0
Lee Magnusson <lee@unhuman.io>
2022-09-14 19:14:35 -0700

    remove multipoll

--- src/motor/motor_thread.cpp
+++ src/motor/motor_thread.cpp
@@ -16,20 +16,20 @@ void MotorThread::update() {
     data_.last_time_start = data_.time_start;
     data_.time_start = std::chrono::steady_clock::now();
     // start a read on all motors
-    motor_manager_.multipoll(1);
+    motor_manager_.poll(); //multipoll(1);
     data_.aread_time = std::chrono::steady_clock::now();
 
     // there is some time before data will return on USB, can do pre update work
     pre_update();
 
     // poll with timeout
-    int retval = motor_manager_.multipoll(poll_timeout_ns_);
-    if (retval != motor_manager_.motors().size()) {
-        if (!user_space_driver_) {
-            // user space driver currently doesn't deal with poll
-            throw std::runtime_error("MotorThread poll error " + std::to_string(retval) + " " + strerror(-retval));
-        }
-    }
+    // int retval = motor_manager_.multipoll(poll_timeout_ns_);
+    // if (retval != motor_manager_.motors().size()) {
+    //     if (!user_space_driver_) {
+    //         // user space driver currently doesn't deal with poll
+    //         throw std::runtime_error("MotorThread poll error " + std::to_string(retval) + " " + strerror(-retval));
+    //     }
+    // }
 
     // blocking io to get the data already set up and wait if not ready yet
     std::memcpy(data_.statuses, motor_manager_.read().data(), sizeof(data_.statuses));
