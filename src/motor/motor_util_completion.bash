12b4a7620c84fe4c1e27c718a4e4c46ee2d68b73
Lee Magnusson <lee@unhuman.io>
2023-04-28 15:47:21 -0700

    lock bash completion

--- src/motor/motor_util_completion.bash
+++ src/motor/motor_util_completion.bash
@@ -29,7 +29,7 @@ _motor_util_completion()
 
     COMREPLY=()
     local words
-    local base_words="-l --list -c --check-messages-version --no-list --list-names-only --list-path-only --list-devpath-only --list-serial-number-only -n --names -p --paths -d --devpaths -s --serial_numbers set read --set-api --api --run-stats -v --version -u --user-space --allow-simulated -h --help";
+    local base_words="-l --list -c --check-messages-version --no-list --list-names-only --list-path-only --list-devpath-only --list-serial-number-only -n --names -p --paths -d --devpaths -s --serial_numbers set read --set-api --api --run-stats -v --version -u --user-space --allow-simulated --lock -h --help";
     case $subcommand in
         set) words="--host_time --mode --current --position --velocity --torque --torque_dot --reserved --gpio state position_tuning current_tuning stepper_tuning voltage stepper_velocity read -h --help";
             case $last in
