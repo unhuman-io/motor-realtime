ea3566b2982418ab6f3468517599448351a662d4
Lee Magnusson <lee@unhuman.io>
2023-05-22 12:30:26 -0700

    haptic bumps

--- example/haptic.cpp
+++ example/haptic.cpp
@@ -1,6 +1,7 @@
 #include "motor_app.h"
 #include "motor_thread.h"
 #include <poll.h>
+#include <cmath>
 
 using namespace obot;
 
@@ -13,9 +14,15 @@ class Task : public MotorThread {
  protected:
 	virtual void controller_update() {
 		motor_manager_.set_command_mode(TORQUE);
-		float torque_desired = -.01 * data_.statuses[0].motor_position;
+		double joint_position = gear_ratio * data_.statuses[0].motor_position;
+		double torque_desired = 0;
 
-		motor_manager_.set_command_torque({torque_desired});
+		for (int i=0; i<num; i++) {
+			double offset = -spacing*(num/2) + i*spacing;
+			torque_desired += -amplitude * exp(-1/width*std::pow(joint_position - offset,2));
+		}
+
+		motor_manager_.set_command_torque({(float) torque_desired});
 	}
 	virtual void post_init() {
 		auto motor = motor_manager_.motors()[0];
@@ -24,6 +31,11 @@ class Task : public MotorThread {
 
  private:
 	double dt_  = 1./2000;
+	double width = .01;
+	double amplitude = 2;
+	double spacing = .1;
+	int num = 5;
+	double gear_ratio = 81;
 };
 
 int main (int argc, char **argv)
