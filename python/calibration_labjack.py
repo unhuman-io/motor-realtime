603c60928b83bdcae2f92ac17810272af20c24de
Lee Magnusson <lee@unhuman.io>
2023-06-01 14:47:15 -0700

    working calibration

--- python/calibration_labjack.py
+++ python/calibration_labjack.py
@@ -19,10 +19,11 @@ print("Opened a LabJack with Device type: %i, Connection type: %i,\n"
       "Serial number: %i, IP address: %s, Port: %i,\nMax bytes per MB: %i" %
       (info[0], info[1], info[2], ljm.numberToIP(info[3]), info[4], info[5]))
 
-dio = {"i+_a": "FIO1", "i-_b": "FIO2", "i-_c": "FIO3", "i-_48v": "FIO3"}
-aio = {"3V3": "AIN8", "5V": "AIN10", "I5V": "AIN13"}
-names = ["AIN13_NEGATIVE_CH", "AIN13_RANGE", "AIN13_RESOLUTION_INDEX", "AIN13_SETTLING_US"]
-aValues = [12, 0.01, 0, 0]
+dio = {"prog": "FIO0", "i+_a": "FIO1", "i-_b": "FIO2", "i-_c": "FIO3", "i-_48v": "FIO3"}
+aio = {"3V3": "AIN8", "5V": "AIN10", "I5V": "AIN12"}
+names = ["AIN12_NEGATIVE_CH", "AIN12_RANGE", "AIN12_RESOLUTION_INDEX", "AIN12_SETTLING_US",
+    "AIN13_NEGATIVE_CH", "AIN13_RANGE", "AIN13_RESOLUTION_INDEX", "AIN13_SETTLING_US"]
+aValues = [13, 0.01, 0, 0, 199, 0.01, 0, 0]
 numFrames = len(names)
 ljm.eWriteNames(handle, numFrames, names, aValues)
 
@@ -58,8 +59,11 @@ assert abs(v5V_measured - 5) < .1
 print("5V voltage measured {}".format(v5V_measured))
 
 # measure 5V current
-i5V_measured = ljm.eReadName(handle, aio["I5V"])
-assert abs(i5V_measured - .12) < .05
+i5V_measured = ljm.eReadName(handle, aio["I5V"])*(-100)
+#unprogrammed
+#assert abs(i5V_measured - .03) < .02
+#programmed
+assert abs(i5V_measured - .07) < .02
 print("5V current measured {}".format(i5V_measured))
 
 # measure 3v3 voltage
@@ -67,7 +71,13 @@ v3v3_measured = ljm.eReadName(handle, aio["3V3"])
 assert abs(v3v3_measured - 3.3) < .1
 print("3v3 voltage measured {}".format(v3v3_measured))
 
-# program
+
+# input("power off 5V")
+# ljm.eWriteName(handle, dio["prog"], 1)
+
+# input("power on 5V")
+# # program
+# ljm.eWriteName(handle, dio["prog"], 0)
 # firmware_tools.program()
 
 # connect to motor
@@ -103,16 +113,27 @@ assert abs(v3v3_calibration - 1) < .1
 #output_dict["v3v3_calibration"] = "{}".format(v3v3_calibration)
 
 # measure 5V current 
-i5V_measured = ljm.eReadName(handle, aio["I5V"])/.01
-assert abs(i5V_measured - .12) < .05
+i5V_measured = -ljm.eReadName(handle, aio["I5V"])/.01
+
 
 # motor read 5V current
 i5V_read = api_average(test_motor["i5V"], 1000)
 print("5V current measured {}, read {}".format(i5V_measured, i5V_read))
+assert abs(i5V_measured - .077) < .02
+
+# read temperatures
+Tboard = api_average(test_motor["Tboard"], 1000)
+Tbridge = api_average(test_motor["Tbridge"], 1000)
+Tbridge2 = api_average(test_motor["Tbridge2"], 1000)
+print("Temperatures Tboard: {}, Tbridge: {}, Tbridge2: {}".format(Tboard, Tbridge, Tbridge2))
+assert abs(Tboard - 30) < 20
+assert abs(Tboard - Tbridge) < 3
+assert abs(Tboard - Tbridge) < 3
+
 
 # calibrate 5V current
-i5V_calibration = i5V_measured/i5V_read
-assert abs(i5V_calibration - 1) < .1
+#i5V_calibration = i5V_measured/i5V_read
+#assert abs(i5V_calibration - 1) < .1
 #output_dict["i5V_gain"] = "1000 * {}".format(i5V_calibration)
 
 ps48V.set_voltage(48)
@@ -142,18 +163,21 @@ i48V = ps48V.get_current()
 print("48V current PWM enabled: {}".format(i48V))
 
 # zero current sensors
-test_motor["zero_current_sensors"] = "4"
-time.sleep(4.1)
+test_motor["zero_current_sensors"] = "8"
+time.sleep(8.5)
 def record_current_sensor_bias(name):
     bias = float(test_motor[name + "_bias"].get())
     assert abs(bias) < 10
-    output_dict["fast_loop_param"][name + "_bias"] = "{}".format(bias)
+    output_dict["fast_loop_param"][name + "_bias"] = "{:.3f}".format(bias)
 
 record_current_sensor_bias("ia")
 record_current_sensor_bias("ib")
 record_current_sensor_bias("ic")
 
 # set mode damped and run 10A current through phases
+m.set_command_mode(motor.ModeDesired.Open)
+m.write_saved_commands()
+time.sleep(.1)
 m.set_command_mode(motor.ModeDesired.Damped)
 m.write_saved_commands()
 
@@ -165,7 +189,8 @@ ps10A.set_voltage(3)
 ps10A.set_current(10)
 ps10A.set_on()
 
-time.sleep(.5)
+time.sleep(2)
+#input("enter to continue")
 
 def record_current_sensor_calibration(name):
     name_map={"ia": "adc1", "ib": "adc2", "ic": "adc3"}
@@ -173,36 +198,43 @@ def record_current_sensor_calibration(name):
     print("{} read: {}".format(name, current))
     if name != "i48V":
         assert abs(abs(current) - 10) < 1
-        output_dict["fast_loop_param"][name_map[name] + "_gain"] = "-3.3/4096/(.0005*10)*{}".format(10/abs(current))
+        output_dict["fast_loop_param"][name_map[name] + "_gain"] = "-3.3/4096/(.0005*10)*{:.3f}".format(10/abs(current))
+        output_dict["fast_loop_param"][name+"_bias"] = output_dict["fast_loop_param"][name+"_bias"] + "*{:.3f}".format(10/abs(current))
     else:
         assert abs(abs(current) - 5) < 1
 
 record_current_sensor_calibration("ia")
 record_current_sensor_calibration("ib")
 
+ps10A.set_off()
+time.sleep(.5)
+
 names = [dio["i+_a"], dio["i-_b"], dio["i-_c"]]
 aValues = [1, 0, 1]
 numFrames = len(names)
 ljm.eWriteNames(handle, numFrames, names, aValues)
-time.sleep(.5)
+
+ps10A.set_on()
+time.sleep(2)
+#input("enter to continue")
 
 record_current_sensor_calibration("ic")
 
 
 # set mode damped high
-m.set_command_mode(motor.ModeDesired.Voltage) # 50% duty cycle voltage
-m.write_saved_commands()
+# m.set_command_mode(motor.ModeDesired.Voltage) # 50% duty cycle voltage
+# m.write_saved_commands()
 
-names = [dio["i+_a"], dio["i-_b"], dio["i-_c"], dio["i-_48v"]]
-aValues = [1, 0, 0, 1]
-numFrames = len(names)
-ljm.eWriteNames(handle, numFrames, names, aValues)
-time.sleep(.5)
+# names = [dio["i+_a"], dio["i-_b"], dio["i-_c"], dio["i-_48v"]]
+# aValues = [1, 0, 0, 1]
+# numFrames = len(names)
+# ljm.eWriteNames(handle, numFrames, names, aValues)
+# time.sleep(.5)
 
-record_current_sensor_calibration("i48V")
+#record_current_sensor_calibration("i48V")
 
 
-#rigol1.off()
+rigol1.off()
 
 names = [dio["i+_a"], dio["i-_b"], dio["i-_c"], dio["i-_48v"]]
 aValues = [0, 0, 0, 0]
