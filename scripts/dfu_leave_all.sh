59679e28641803a42b1f56ab5aa7bd56d6b569a9
Lee Magnusson <lee@unhuman.io>
2024-01-17 10:06:41 -0800

    Improve leave all

--- scripts/dfu_leave_all.sh
+++ scripts/dfu_leave_all.sh
@@ -4,6 +4,6 @@ serials=$(dfu-util -l | grep "@Internal Flash" | sed -nr 's/.*serial="([^"]*)"/\
 
 for serial in $serials; do
     rm -f tmp1.dat
-    dfu-util -a0 -s 0x8060000:leave -U tmp1.dat -S $serial
+    dfu-util -a0 -s 0x8000000:8:leave -U tmp1.dat -S $serial
     rm tmp1.dat
 done
