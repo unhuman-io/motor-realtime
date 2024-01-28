fa1a94a8fe462014b66a0f382cb9808ad0bf433a
Lee Magnusson <lee@unhuman.io>
2024-01-28 06:27:23 -0800

    some working textapi

--- include/motor_uart.h
+++ include/motor_uart.h
@@ -5,6 +5,19 @@
 
 namespace obot {
 
+class Mailbox : public TextFile {
+ public:
+    virtual ssize_t read(char * data, unsigned int length) override;
+    virtual ssize_t write(const char * data, unsigned int length) override;
+    virtual ssize_t writeread(const char * data_out, unsigned int length_out, char * data_in, unsigned int length_in) override;
+    uint8_t send_mailbox_ = 2; // text api
+    uint8_t recv_mailbox_ = 1; // text api
+    int fd_;
+ private:
+    uint32_t read_error_ = 0;
+    uint32_t write_error_ = 0;
+};
+
 class MotorUART : public Motor {
  public:
     MotorUART(std::string dev_path, uint32_t baud_rate = 4000000);
@@ -18,10 +31,9 @@ class MotorUART : public Motor {
     int sync();
 
  private:
-    uint32_t read_error_ = 0;
-    uint32_t write_error_ = 0;
-    uint32_t sync_error_ = 0;
+    Mailbox realtime_mailbox_;
     struct pollfd pollfds_[1] = {};
+    uint32_t sync_error_ = 0;
 };
 
 }; // namespace obot
--- src/motor/motor_uart.cpp
+++ src/motor/motor_uart.cpp
@@ -14,8 +14,14 @@ MotorUART::MotorUART(std::string dev_path, uint32_t baud_rate) {
   if (result < 0) {
     throw std::runtime_error("Error opening " + dev_path_ + " error " + std::to_string(errno) + ": " + strerror(errno));
   }
+  realtime_mailbox_.send_mailbox_ = 4;
+  realtime_mailbox_.recv_mailbox_ = 3;
+  realtime_mailbox_.fd_ = fd_;
   pollfds_[0].fd = fd_;
   pollfds_[0].events = POLLIN;
+  motor_txt_ = std::move(std::unique_ptr<Mailbox>(new Mailbox()));
+  Mailbox * text_mailbox = static_cast<Mailbox *>(motor_txt_.get());
+  text_mailbox->fd_ = fd_;
   // only one item can access uart devices due to protocol
   result = lock();
   if (result < 0) {
@@ -62,76 +68,11 @@ void MotorUART::set_baud_rate(uint32_t baud_rate) {
 }
 
 ssize_t MotorUART::read() {
-  int retval;
-  const uint8_t read_command[4] = {0xF0, 0x0F, 3, 0};
-  retval = ::write(fd_, read_command, sizeof(read_command));
-  if (retval != sizeof(read_command)) {
-    read_error_++;
-    return retval;
-  }
-  uint8_t ack_len[3];
-  std::this_thread::sleep_for(std::chrono::milliseconds(10));
-  retval = ::read(fd_, ack_len, sizeof(ack_len));
-  if (retval != sizeof(ack_len)) {
-    read_error_++;
-    return retval;
-  }
-  std::cout << "ack " << std::hex << +ack_len[0] << " " << +ack_len[1] << " " << +ack_len[2] << 
-      " sizeof status " << sizeof(status_) << " read error " << read_error_ << std::dec << std::endl;
-  if (ack_len[0] == 0x79 && ack_len[1] == 0x79 && ack_len[2] == 0x3b) {
-    Status status_tmp;
-    retval = ::read(fd_, &status_tmp, sizeof(status_tmp));
-    if (retval != sizeof(status_tmp)) {
-      read_error_++;
-      return retval;
-    }
-    retval = ::read(fd_, ack_len, 2);
-    if (retval != 2) {
-      read_error_++;
-      return retval;
-    }
-    if (ack_len[1] == 0x79) {
-      status_ = status_tmp;
-      return sizeof(status_);
-    }
-  }
-  read_error_++;
-  return 0;
+  return realtime_mailbox_.read((char *) &status_, sizeof(status_));
 }
 
 ssize_t MotorUART::write() {
-  int retval;
-  const uint8_t write_command[5] = {0xF1, 0x0E, 4, sizeof(command_)-1, 0};
-  retval = ::write(fd_, write_command, sizeof(write_command));
-  if (retval != sizeof(write_command)) {
-    write_error_++;
-    return retval;
-  }
-  uint8_t ack_len[2];
-  std::this_thread::sleep_for(std::chrono::milliseconds(1));
-  retval = ::read(fd_, ack_len, sizeof(ack_len));
-  if (retval != sizeof(ack_len)) {
-    write_error_++;
-    return retval;
-  }
-  retval = ::write(fd_, &command_, sizeof(command_));
-  if (retval != sizeof(command_)) {
-    write_error_++;
-    return retval;
-  }
-  uint8_t checksum[1] = {};
-  retval = ::write(fd_, checksum, sizeof(checksum));
-  if (retval != sizeof(checksum)) {
-    write_error_++;
-    return retval;
-  }
-  std::this_thread::sleep_for(std::chrono::milliseconds(10));
-  retval = ::read(fd_, ack_len, 1);
-  if (retval != 1) {
-    write_error_++;
-    return retval;
-  }
-  return sizeof(command_);
+  return realtime_mailbox_.write((char *) &command_, sizeof(command_));
 }
 
 int MotorUART::sync() {
@@ -153,7 +94,7 @@ int MotorUART::sync() {
     } else if (retval > 0) {
       retval = ::read(fd_, ack, sizeof(ack));
       if (retval != sizeof(ack)) {
-        write_error_++;
+        sync_error_++;
         return retval;
       } else {
         return 0;
@@ -162,6 +103,86 @@ int MotorUART::sync() {
   }
   errno = EIO;
   return -1;
+}
+
+ssize_t Mailbox::read(char * data, unsigned int length) {
+  int retval;
+  const uint8_t read_command[4] = {0xF0, 0x0F, recv_mailbox_, 0};
+  retval = ::write(fd_, read_command, sizeof(read_command));
+  if (retval != sizeof(read_command)) {
+    read_error_++;
+    return retval;
+  }
+  uint8_t ack_len[3];
+  std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  retval = ::read(fd_, ack_len, sizeof(ack_len));
+  if (retval != sizeof(ack_len)) {
+    read_error_++;
+    return retval;
+  }
+  // std::cout << "ack " << std::hex << +ack_len[0] << " " << +ack_len[1] << " " << +ack_len[2] 
+  //   << " read error " << read_error_ << std::dec << std::endl;
+  if (ack_len[0] == 0x79 && ack_len[1] == 0x79) {
+    length = ack_len[2] + 1;
+    retval = ::read(fd_, data, length);
+    if (retval != length) {
+      read_error_++;
+      return retval;
+    }
+    retval = ::read(fd_, ack_len, 2);
+    if (retval != 2) {
+      read_error_++;
+      return retval;
+    }
+    if (ack_len[1] == 0x79) {
+      return length;
+    }
+  }
+  read_error_++;
+  return 0;
+}
+
+ssize_t Mailbox::write(const char * data, unsigned int length) {
+  int retval;
+  const uint8_t write_command[5] = {0xF1, 0x0E, send_mailbox_, (uint8_t) (length-1), 0};
+  retval = ::write(fd_, write_command, sizeof(write_command));
+  if (retval != sizeof(write_command)) {
+    write_error_++;
+    return retval;
+  }
+  uint8_t ack_len[2];
+  std::this_thread::sleep_for(std::chrono::milliseconds(1));
+  retval = ::read(fd_, ack_len, sizeof(ack_len));
+  if (retval != sizeof(ack_len)) {
+    write_error_++;
+    return retval;
+  }
+  retval = ::write(fd_, data, length);
+  if (retval != length) {
+    write_error_++;
+    return retval;
+  }
+  uint8_t checksum[1] = {};
+  retval = ::write(fd_, checksum, sizeof(checksum));
+  if (retval != sizeof(checksum)) {
+    write_error_++;
+    return retval;
+  }
+  std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  retval = ::read(fd_, ack_len, 1);
+  if (retval != 1) {
+    write_error_++;
+    return retval;
+  }
+  return length;
+}
+
+ssize_t Mailbox::writeread(const char * data_out, unsigned int length_out, char * data_in, unsigned int length_in) {
+  int write_result = write(data_out, length_out);
+  if (write_result < 0) {
+    return write_result;
+  }
+  return read(data_in, length_in);
 }
 
 }; // namespace obot
