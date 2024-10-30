659340ae767e5bb4328e5deec06284770ae40427
Lee Magnusson <lee@unhuman.io>
2024-10-30 10:11:04 -0700

    invalid command fault

--- src/motor_python.cpp
+++ src/motor_python.cpp
@@ -39,6 +39,7 @@ static py::dict motor_error_dict(const MotorError &e)
     d["encoder_disagreement"] = e.encoder_disagreement;
     d["torque_sensor_disagreement"] = e.torque_sensor_disagreement;
     d["init_failure"] = e.init_failure;
+    d["invalid_command"] = e.invalid_command;
     d["motor_encoder_warning"] = e.output_encoder_warning;
     d["output_encoder_warning"] = e.output_encoder_warning;
     d["torque_sensor_warning"] = e.torque_sensor_warning;
@@ -72,6 +73,7 @@ static MotorError dict_to_motor_error(py::dict d) {
     e.encoder_disagreement = d["encoder_disagreement"].cast<bool>();
     e.torque_sensor_disagreement = d["torque_sensor_disagreement"].cast<bool>();
     e.init_failure = d["init_failure"].cast<bool>();
+    e.invalid_command = d["invalid_command"].cast<bool>();
     e.motor_encoder_warning = d["motor_encoder_warning"].cast<bool>();
     e.output_encoder_warning = d["output_encoder_warning"].cast<bool>();
     e.torque_sensor_warning = d["torque_sensor_warning"].cast<bool>();
