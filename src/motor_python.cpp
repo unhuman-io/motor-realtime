56e736e62452ab596a6e42e3b9dd82d6529b6992
Lee Magnusson <lee@unhuman.io>
2024-11-06 13:06:30 -0800

    fix  motor encoder warning typo

--- src/motor_python.cpp
+++ src/motor_python.cpp
@@ -40,7 +40,7 @@ static py::dict motor_error_dict(const MotorError &e)
     d["torque_sensor_disagreement"] = e.torque_sensor_disagreement;
     d["init_failure"] = e.init_failure;
     d["invalid_command"] = e.invalid_command;
-    d["motor_encoder_warning"] = e.output_encoder_warning;
+    d["motor_encoder_warning"] = e.motor_encoder_warning;
     d["output_encoder_warning"] = e.output_encoder_warning;
     d["torque_sensor_warning"] = e.torque_sensor_warning;
     d["motor_current_limit"] = e.motor_current_limit;
