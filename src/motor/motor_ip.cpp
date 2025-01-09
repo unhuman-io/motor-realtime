4842029ea9a932c8bc0266cfd75c84bf571518c8
Lee Magnusson <lee@unhuman.io>
2025-01-09 14:08:39 -0800

    fix status packet parsing to control packets

--- src/motor/motor_ip.cpp
+++ src/motor/motor_ip.cpp
@@ -196,9 +196,10 @@ ssize_t UDPFile::_read(char * data, unsigned int length, bool write_read) {
 ssize_t UDPFile::read(char * data, unsigned int length, bool write_read) {
   ssize_t retval = _read(data, length, write_read);
   
-  if (retval >= sizeof(APIControlPacket) && data[0] == 0) {
-      // a control packet
-      APIControlPacket * packet = reinterpret_cast<APIControlPacket *>(data);
+  if (api_mode_) {
+    if (retval >= sizeof(APIControlPacket) && data[0] == 0) {
+        // a control packet
+        APIControlPacket * packet = reinterpret_cast<APIControlPacket *>(data);
       if (packet->type == TIMEOUT_REQUEST) {
           // timeout request
           if (retval == sizeof(APIControlPacket)) {
@@ -232,8 +233,9 @@ ssize_t UDPFile::read(char * data, unsigned int length, bool write_read) {
               std::memmove(data_ptr, data_ptr + header_size, retval - header_size);
               // ignoring packet_number
           }
-          retval = total_count_received;
-      }
+            retval = total_count_received;
+        }
+    }
   }
   return retval;
 }
