10f181a7f534787343b411462a8ae00e21f5bafe
leemagnusson <leemagnusson@gmail.com>
2025-01-23 15:22:47 -0800

    Feature/pub sub (#172)
    * python pub sub example
    
    * add pub sub build
    
    * publisher subscriber stuff
    
    * some todo comments
    
    * doc notes
    
    * arm builds having issues
    
    ---------
    
    Co-authored-by: Lee Magnusson <lee@unhuman.io>

---
+++ example/python/pub_sub/motor_pub_sub.cpp
@@ -0,0 +1,44 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include "motor_publisher.h"
+#include "motor_subscriber.h"
+
+namespace py = pybind11;
+using namespace obot;
+
+PYBIND11_MODULE(motor_pub_sub, m)
+{
+    m.doc() = "Motor publisher and subscriber example";
+
+    struct Data {
+        double pos_x;
+        double pos_y;
+        double pos_z;
+    };
+    struct Command{
+        double force_x;
+        double force_y;
+        double force_z;
+    };
+
+    py::class_<Data>(m, "Data")
+        .def(py::init())
+        .def_readwrite("pos_x", &Data::pos_x)
+        .def_readwrite("pos_y", &Data::pos_y)
+        .def_readwrite("pos_z", &Data::pos_z);
+
+    py::class_<Command>(m, "Command")
+        .def(py::init())
+        .def_readwrite("force_x", &Command::force_x)
+        .def_readwrite("force_y", &Command::force_y)
+        .def_readwrite("force_z", &Command::force_z);
+
+    py::class_<MotorPublisher<Command>>(m, "MotorPublisher")
+        .def(py::init<std::string>())
+        .def("publish", &MotorPublisher<Command>::publish);
+
+    py::class_<MotorSubscriber<Data>>(m, "MotorSubscriber")
+        .def(py::init<std::string>())
+        .def("read", &MotorSubscriber<Data>::read)
+        .def("get_dt", &MotorSubscriber<Data>::get_dt);
+}
--- include/motor_subscriber.h
+++ include/motor_subscriber.h
@@ -8,29 +8,45 @@
 #include <cstring>
 #include <semaphore.h>
 #include <cstack.h>
+#include "motor_publisher.h"
 
 namespace obot {
 
 template <class T>
 class MotorSubscriber {
  public:
+    using Data = typename MotorPublisher<T>::Data;
     MotorSubscriber(std::string shm_name = "motor_data") : shm_name_(shm_name) {
         open();
     }
     ~MotorSubscriber() {
         munmap(memptr_, sizeof(*data_));
         close(fd_);
+        shm_unlink(shm_name_.c_str());
     }
     T read() {
-        T data = {};
+        Data data = {};
+        update_stats(data.header);
         if (fd_ > 0) {
             data = data_->top();
         } else {
             open();
         }
-        return data;
+        return data.data;
+    }
+    uint64_t get_dt() const {
+        return dt_;
     }
  private:
+    void update_stats(typename MotorPublisher<T>::Header & header) {
+        if (header.seq_num > last_seq_num_) {
+            last_seq_num_ = header.seq_num;
+            last_timestamp_ = header.timestamp;
+        }
+        dt_ = (header.timestamp - last_timestamp_)/(header.seq_num - last_seq_num_);
+        last_timestamp_ = header.timestamp;
+        last_seq_num_ = header.seq_num;
+    }
     void open() {
         fd_ = shm_open(shm_name_.c_str(), O_RDONLY, 0666);
         if (fd_ > 0) {
@@ -40,13 +56,16 @@ class MotorSubscriber {
                         MAP_SHARED, /* mapping visible to other processes */
                         fd_,         /* file descriptor */
                         0);
-            data_ = reinterpret_cast<CStack<T> *>(memptr_);
+            data_ = reinterpret_cast<CStack<Data> *>(memptr_);
         }
     }
     int fd_;
     std::string shm_name_;
     void * memptr_;
-    CStack<T> * data_;
+    CStack<Data> * data_;
+    uint64_t last_timestamp_ = 0;
+    uint64_t last_seq_num_ = 0;
+    uint64_t dt_ = 0;  
 };
 
 }  // namespace obot
