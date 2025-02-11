6f11b69b73ead8a3da05f9444ff752ddf603a6c9
Lee Magnusson <lee@unhuman.io>
2025-02-11 13:01:13 -0800

    raw packet printer

--- src/motor/motor_util.cpp
+++ src/motor/motor_util.cpp
@@ -15,6 +15,7 @@
 #include "keyboard.h"
 #include "motor_util_fun.h"
 #include <json.hpp>
+#include "protocol_parser.h"
 
 using namespace obot;
 
@@ -96,6 +97,8 @@ int main(int argc, char** argv) {
     std::vector<std::string> uart_paths = {};
     std::vector<std::string> can_devs = {"any"};
     bool uart_raw = false;
+    bool print_raw_packet = false;
+    bool parse_raw_packet = false;
     std::vector<std::string> ips = {};
     
     std::string config_dir = get_config_dir();
@@ -227,6 +230,8 @@ int main(int argc, char** argv) {
     auto run_stats_option = app.add_option("--run-stats", run_stats, "Check firmware run timing")->type_name("NUM_SAMPLES")->expected(0,1)->capture_default_str();
     auto set_timeout_option = app.add_option("--set-timeout", timeout_ms, "Set timeout in ms")->expected(0,1)->capture_default_str();
     auto can_option = app.add_option("-f,--can", can_devs, "Connect to CAN_DEVS(S)")->type_name("CAN_DEV")->expected(0,-1)->capture_default_str();
+    app.add_flag("--print-raw-packet", print_raw_packet, "Print raw packet only. Doesn't connect to motors");
+    app.add_flag("--parse-raw-packet", parse_raw_packet, "Parse raw packet only from stdin. Doesn't connect to motors")->needs(ip_option);
     CLI11_PARSE(app, argc, argv);
 
     signal(SIGINT,[](int /* signum */){ signal_exit = true; });
@@ -240,6 +245,97 @@ int main(int argc, char** argv) {
         read_opts.host_time = true;
         no_list = true;
         no_print_unconnected = true;
+    }
+
+    if (print_raw_packet) {
+        // todo move ip specific stuff to motor_ip.cpp
+        uint8_t packet_id;
+        uint8_t *packet_data;
+        uint8_t length;
+        if (*set_api) {
+            // ascii packet
+            packet_id = 4;
+            packet_data = (uint8_t*)set_api_data[0].c_str();
+            length = set_api_data[0].size();
+        } else if (*set) {
+            // command packet
+            packet_id = 1;
+            packet_data = (uint8_t*)&command;
+            length = sizeof(command);
+        } else {
+            std::cerr << "Error: --print-raw-packet requires set or --set-api" << std::endl;
+            exit(1);
+        }
+        if (*ip_option) {
+            uint8_t length_out;
+            uint8_t buffer[128];
+            figure::ProtocolParser parser(buffer, sizeof(buffer));
+            packet_data = parser.generatePacket(packet_data, length, packet_id, &length_out);
+            length = length_out;
+        }
+        write(1, packet_data, length);
+        exit(0);
+    }
+
+    if (parse_raw_packet) {
+        // only makes sense with the ip option
+        if (!*ip_option) {
+            std::cerr << "Error: --parse-raw-packet requires --ips" << std::endl;
+            exit(1);
+        }
+        uint8_t buffer[128];
+        figure::ProtocolParser parser(buffer, sizeof(buffer));
+        parser.registerCallback(1, [](const uint8_t* packet, uint16_t length){
+            std::vector<Command> commands;
+            commands.push_back(*reinterpret_cast<const Command*>(packet));
+            std::cout << "command: " << commands << std::endl;
+        });
+        parser.registerCallback(2, [](const uint8_t* packet, uint16_t length){
+            std::vector<Status> statuses;
+            statuses.push_back(*reinterpret_cast<const Status*>(packet));
+            std::cout << "status: " << statuses << std::endl;
+        });
+        parser.registerCallback(3, [](const uint8_t* packet, uint16_t length){
+            std::vector<Command> commands;
+            commands.push_back(*reinterpret_cast<const Command*>(packet));
+            std::cout << "command_status: " << commands << std::endl;
+        });
+        parser.registerCallback(4, [](const uint8_t* packet, uint16_t length){
+            std::string s(reinterpret_cast<const char*>(packet), length);
+            std::cout << "api command: " << s << std::endl;
+        });
+        parser.registerCallback(5, [](const uint8_t* packet, uint16_t length){
+            std::string s(reinterpret_cast<const char*>(packet), length);
+            std::cout << "api response: " << s << std::endl;
+        });
+        int current_idx = 0;
+        while(signal_exit == false) {
+            // fill and parse a circular buffer
+            if (current_idx == sizeof(buffer)) {
+                current_idx = 0;
+            }
+            struct pollfd fds[1];
+            fds[0].fd = 0;
+            ssize_t retval = poll(fds, 1, 10);
+            if (retval < 0) {
+                std::cerr << "Error: poll failed" << std::endl;
+                exit(1);
+            } else if (retval == 0) {
+                continue;
+            }
+            retval = read(0, buffer, sizeof(buffer) - current_idx);
+            if (retval < 0) {
+                std::cerr << "Error: read failed" << std::endl;
+                exit(1);
+            } else if (retval == 0) {
+                // closed pipe
+                break;
+            } else {
+                current_idx += retval;
+                parser.process(current_idx);
+            }
+        }
+        exit(0);
     }
 
     MotorManager m(user_space_driver, check_messages_version);
