cbacf95d8afb8c055ed13d1b4f793de8379b25a7
Lee Magnusson <lee@unhuman.io>
2025-09-27 07:27:16 -0700

    catch exceptions

--- include/exception.h
+++ include/exception.h
@@ -1,12 +1,18 @@
 #pragma once
 #include <stdexcept>
+#include <source_location>
 #include "terminal.h"
 namespace obot {
 
 class RuntimeException : public std::runtime_error {
  public:
-    explicit RuntimeException(const std::string& message)
-        : std::runtime_error(std::string(ANSI_RED_STDERR) + message + std::string(ANSI_RESET_STDERR)) {}
+    explicit RuntimeException(const std::string& message,
+                std::source_location location = std::source_location::current())
+        : std::runtime_error(std::string(ANSI_RED_STDERR) + message + std::string(ANSI_RESET_STDERR)),
+          location_(location) {}
+    std::source_location location() const { return location_;}
+ private:
+    std::source_location location_;
 };
 
 };
