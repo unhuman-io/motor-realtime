d8c7188d877aa0b967b9e5f48ba67326cc467b14
leemagnusson <leemagnusson@gmail.com>
2025-10-02 15:49:01 -0700

    remove use of format (#202)

--- include/exception.h
+++ include/exception.h
@@ -2,7 +2,6 @@
 #include <stdexcept>
 #if __cplusplus >= 202002L
 #include <source_location>
-#include <format>
 #endif
 #include "terminal.h"
 namespace obot {
@@ -16,8 +15,8 @@ class RuntimeException : public std::runtime_error {
           location_(location) {}
     std::source_location location() const { return location_;}
     std::string location_print() const {
-        return std::format(" location(): {}({})\n", location().file_name(), location().line()) +
-               std::format(" function(): {}", location().function_name());
+        return " location(): " + std::string(location().file_name()) + "(" + std::to_string(location().line()) + ")\n" +
+               " function(): " + std::string(location().function_name());
     }
  private:
     std::source_location location_;
