985a0f5d5718b6171f14d4defd0c6c62dc84aefd
leemagnusson <leemagnusson@gmail.com>
2025-10-16 15:57:28 -0700

    allow long packet and timeout (#204)

--- src/motor/motor_ip.cpp
+++ src/motor/motor_ip.cpp
@@ -218,7 +218,7 @@ ssize_t UDPFile::read(char * data, unsigned int length, bool write_read) {
               // retriggers the read with the new timeout
               uint32_t old_timeout_ms = timeout_ms_;
               timeout_ms_ += packet->timeout_request.timeout_us/1000;
-              ssize_t retval = _read(data, length, write_read);
+              ssize_t retval = read(data, length, write_read);
               timeout_ms_ = old_timeout_ms;
               unlock_communication();
               return retval;
