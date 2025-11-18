a7d1798c656bf8d2678fc73d2ac0369e03b0e00e
Lee Magnusson <lee.magnusson@figure.ai>
2025-11-18 13:48:56 -0800

    more commands

--- include/gdbserver.h
+++ include/gdbserver.h
@@ -4,7 +4,8 @@ namespace obot {
 
 class GDBServer {
     public:
-        GDBServer(std::function<std::string(std::string_view)> writeread) : writeread_(writeread) {}
+        GDBServer(std::function<std::string(std::string_view)> writeread);
+        ~GDBServer();
         void start();
         void send_ack() {send_response("+");}
         void send_nack() {send_response("-");}
@@ -12,6 +13,7 @@ class GDBServer {
     private:
         std::function<std::string(std::string_view)> writeread_;
         int connfd_;
+        int sockfd;
 };
 
 }
--- src/motor/gdbserver.cpp
+++ src/motor/gdbserver.cpp
@@ -3,6 +3,7 @@
 #include <netinet/in.h> 
 #include <stdlib.h> 
 #include <string.h> 
+#include <string>
 #include <sys/socket.h> 
 #include <sys/types.h> 
 #include <unistd.h>
@@ -22,19 +23,11 @@ const int PORT = 3293;
 
 namespace obot {
 
-void GDBServer::send_response(std::string response) {
-    std::cout << "sending response: " << response << std::endl;
-    int n = write(connfd_, response.c_str(), response.size());
-    if (n < 0) {
-        throw RuntimeException("write error");
-    }
-}
-
-void GDBServer::start() {
+GDBServer::GDBServer(std::function<std::string(std::string_view)> writeread) : writeread_(writeread) {
     int len; 
     struct sockaddr_in cli; 
     
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
     if (sockfd == -1) { 
         throw RuntimeException("socket creation failed");
     }
@@ -54,15 +47,32 @@ void GDBServer::start() {
     if ((listen(sockfd, 0)) != 0) { 
         throw RuntimeException("Listen failed"); 
     } 
-    std::cout << "server listening" << std::endl;; 
-    len = sizeof(cli); 
-    
-    connfd_ = accept(sockfd, (struct sockaddr*)&cli, (socklen_t*) &len); 
+    std::cout << "server listening" << std::endl;
+
+    len = sizeof(cli);
+        connfd_ = accept(sockfd, (struct sockaddr*)&cli, (socklen_t*) &len); 
     if (connfd_ < 0) { 
         throw RuntimeException("server accept failed..."); 
     }
     std::cout << "server accepted the client" << std::endl;
 
+}
+
+GDBServer::~GDBServer() {
+    std::cout << "closing gdb server sockets" << std::endl;
+    close(connfd_);
+    close(sockfd);
+}
+
+void GDBServer::send_response(std::string response) {
+    std::cout << "sending response: " << response << std::endl;
+    int n = write(connfd_, response.c_str(), response.size());
+    if (n < 0) {
+        throw RuntimeException("write error");
+    }
+}
+
+void GDBServer::start() {
     const int MAX=1000;
     char buf[MAX]; 
     for (;;) {
@@ -92,6 +102,10 @@ void GDBServer::start() {
                 send_ack();
                 ack = true;
             }
+        } else if (str.rfind("\x003", 0) == 0) {
+            //interrupt
+            send_ack();
+            ack = true;
         } else {
             //else "-"
             send_nack();
@@ -102,10 +116,12 @@ void GDBServer::start() {
 
         if (str.rfind("$qSupported", 0) == 0) {
             std::cout << "gdb command: " << str.substr(1) << std::endl;
-            response = "read+;write+;";
+            response = "read+;write+";//;QNonStop+";
         } else if (str.rfind("$g", 0) == 0) {
             std::cout << "gdb command: " << str.substr(1) << std::endl;
-            response = std::string(17*4*2, '0');
+            auto item = writeread_("$g");
+            response = item;
+            //response = std::string(17*4*2, '0');
             //response = "000000004aff7f40000000000000000000000000000000000000000000000000000000000000000044f10b000000000000000000a0fd7f400000000038ab000000000001";
         } else if (str.rfind("$?", 0) == 0) {
             std::cout << "gdb command: " << str.substr(1) << std::endl;
@@ -130,6 +146,33 @@ void GDBServer::start() {
             std::string_view memory_write = str.substr(0, str.size() - 3);
             std::cout << "gdb command: " << memory_write << std::endl;
             response = writeread_(memory_write);
+        } else if (str.rfind("$P", 0) == 0) {
+            std::string_view reg_write = str.substr(0, str.size() - 3);
+            std::cout << "gdb reg write command: " << reg_write << std::endl;
+            response = writeread_(reg_write);
+        } else if (str.rfind("$QNonStop", 0) == 0) {
+            std::cout << "gdb non stop command: " << str.substr(0) << std::endl;
+            response = "OK";
+        } else if (str.rfind("$c", 0) == 0) {
+            std::cout << "gdb c command: " << str.substr(0) << std::endl;
+            auto item = writeread_("$c");
+            std::cout << "item response: " << item << std::endl;
+            // no response
+            continue;
+        } else if (str.rfind("\x003", 0) == 0) {
+            std::cout << "gdb interrupt command" << std::endl;
+            std::string_view interrupt = "$b";
+            auto item = writeread_(interrupt);
+            std::cout << "item response: " << item << std::endl;
+            response = "S05";
+        } else if (str.rfind("$s", 0) == 0) {
+            std::cout << "gdb si command" << std::endl;
+            auto item = writeread_("$s");
+            std::cout << "item response: " << item << std::endl;
+            response = "S05";
+        } else if (str.rfind("$p", 0) == 0) {
+            std::cout << "gdb p command" << str.substr(1) << std::endl;
+            response = writeread_(str.substr(0, str.size() - 3));
         } else {
             response = "";
         }
@@ -155,6 +198,6 @@ void GDBServer::start() {
 
          
     } 
-    close(sockfd);
+
 }
 } // namespace obot
